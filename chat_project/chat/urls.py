c55d50f7506c1ba2140e3c2051548cb458466c85
Orifjon94 <avazbekoripov94@gmail.com>
2021-08-17 18:18:45 +0500

    Chat is working. All rights reserved

--- chat_project/chat/urls.py
+++ chat_project/chat/urls.py
@@ -17,4 +17,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
+    path('<str:room>/', views.room, name="room"),
+    path('checkview', views.checkview, name="checkview"),
+    path('send', views.send, name="send"),
+    path('getMessages/<str:room>/', views.getMessages, name="getMessages")
 ]
